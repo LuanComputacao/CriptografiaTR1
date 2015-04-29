eb5812e1b9a7345e23d46160fa3dce8ea002497f
Luan Roger Santos Santana <luanengcomputacao@gmail.com>
2015-04-29 02:25:24 -0300

    Getters and setters Another test case make cipher

--- src/com/luancomputacao/cifras/Playfair.java
+++ src/com/luancomputacao/cifras/Playfair.java
@@ -1,17 +1,20 @@
 package com.luancomputacao.cifras;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by luan on 27/04/15.
+ *
+ * This class is to cipher a text with Playfair method
  */
 public class Playfair {
     private String keyword;
     private String textoClaro;
     private char[][] matriz = new char[5][5];
     private char alfabeto[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    private String strTextoPreparado;
+    private String strTextoPreparado = null;
     private Character x = 'x';
     private List<Character> lstTextoPreparado = new ArrayList<Character>();
 
@@ -19,31 +22,73 @@ public class Playfair {
     public Playfair(String keyword, String textoClaro) {
         this.keyword = keyword.toLowerCase().replace("\\s+", "");
         this.textoClaro = textoClaro;
-        montaMatriz();
+        this.montaMatriz();
         this.preparaTextoClaro();
+        this.cifrarTexto();
+    }
+
+    /*------------------------------------------------------------------------------------------------------------------
+    | Getters e setters
+    ------------------------------------------------------------------------------------------------------------------*/
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getTextoClaro() {
+        return textoClaro;
+    }
+
+    public void setTextoClaro(String textoClaro) {
+        this.textoClaro = textoClaro;
     }
 
     public char[][] getMatriz() {
         return this.matriz;
     }
 
-    public String getStrTextoPreparado() {
+    public char[] getAlfabeto() {
+        return alfabeto;
+    }
+
+    private void setStrTextoPreparado() {
         this.strTextoPreparado = lstTextoPreparado.toString().replaceAll("[\\n, \\[\\]]", "");
+    }
+
+    public List<Character> getLstTextoPreparado() {
+        return lstTextoPreparado;
+    }
+
+    public void setLstTextoPreparado(List<Character> lstTextoPreparado) {
+        this.lstTextoPreparado = lstTextoPreparado;
+    }
+
+    public String getStrTextoPreparado() {
+        this.setStrTextoPreparado();
         return this.strTextoPreparado;
     }
+
+     /*------------------------------------------------------------------------------------------------------------------
+    | Metodos
+    ------------------------------------------------------------------------------------------------------------------*/
 
     private void montaMatriz() {
         char[] arrKeyword = this.keyword.toCharArray();
         ArrayList<Character> lstKeyword = new ArrayList<Character>();
-        for (int count = 0; count < arrKeyword.length; count++) {
-            if (!lstKeyword.contains(arrKeyword[count])) {
-                lstKeyword.add(arrKeyword[count]);
+
+        for (char anArrKeyword : arrKeyword) {
+            if (!lstKeyword.contains(anArrKeyword)) {
+                lstKeyword.add(anArrKeyword);
             }
         }
 
-        for (int count = 0; count < alfabeto.length; count++) {
-            if (!lstKeyword.contains(alfabeto[count])) {
-                lstKeyword.add(alfabeto[count]);
+        for (char anAlfabeto : alfabeto) {
+            if (!lstKeyword.contains(anAlfabeto)) {
+                lstKeyword.add(anAlfabeto);
             }
         }
 
@@ -67,7 +112,11 @@ public class Playfair {
                 lstTextoPreparado.add(strTemp.charAt(i + 1));
             }
         }
-        System.out.println();
-        this.strTextoPreparado = strTemp.toString();
+        this.strTextoPreparado = strTemp;
+    }
+
+    private void cifrarTexto() {
+        List<char[]> lstMatriz = Arrays.asList(this.matriz);
+        System.out.println(lstMatriz);
     }
 }
