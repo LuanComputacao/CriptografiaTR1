83f5af9274e67d99ae71cde75d068b7dabf94bd0
luansantanaLKD <luan.santana@lojaskd.com.br>
2015-04-29 12:49:32 -0300

    gitignore

--- src/com/luancomputacao/cifras/Playfair.java
+++ src/com/luancomputacao/cifras/Playfair.java
@@ -6,36 +6,89 @@ import java.util.List;
 
 /**
  * Created by luan on 27/04/15.
+ *
+ * This class is to cipher a text with Playfair method
  */
 public class Playfair {
     private String keyword;
-    private String text;
+    private String textoClaro;
     private char[][] matriz = new char[5][5];
     private char alfabeto[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private String strTextoPreparado = null;
+    private Character x = 'x';
+    private List<Character> lstTextoPreparado = new ArrayList<Character>();
 
 
-    public Playfair(String keyword, String text) {
+    public Playfair(String keyword, String textoClaro) {
         this.keyword = keyword.toLowerCase().replace("\\s+", "");
-        this.text = text;
-        fazMatriz();
+        this.textoClaro = textoClaro;
+        this.montaMatriz();
+        this.preparaTextoClaro();
+        this.cifrarTexto();
+    }
+
+    /*------------------------------------------------------------------------------------------------------------------
+    | Getters e setters
+    ------------------------------------------------------------------------------------------------------------------*/
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getTextoClaro() {
+        return textoClaro;
+    }
+
+    public void setTextoClaro(String textoClaro) {
+        this.textoClaro = textoClaro;
     }
 
     public char[][] getMatriz() {
         return this.matriz;
     }
 
-    private void fazMatriz() {
+    public char[] getAlfabeto() {
+        return alfabeto;
+    }
+
+    private void setStrTextoPreparado() {
+        this.strTextoPreparado = lstTextoPreparado.toString().replaceAll("[\\n, \\[\\]]", "");
+    }
+
+    public List<Character> getLstTextoPreparado() {
+        return lstTextoPreparado;
+    }
+
+    public void setLstTextoPreparado(List<Character> lstTextoPreparado) {
+        this.lstTextoPreparado = lstTextoPreparado;
+    }
+
+    public String getStrTextoPreparado() {
+        this.setStrTextoPreparado();
+        return this.strTextoPreparado;
+    }
+
+     /*------------------------------------------------------------------------------------------------------------------
+    | Metodos
+    ------------------------------------------------------------------------------------------------------------------*/
+
+    private void montaMatriz() {
         char[] arrKeyword = this.keyword.toCharArray();
         ArrayList<Character> lstKeyword = new ArrayList<Character>();
-        for (int count = 0; count < arrKeyword.length; count++) {
-            if (!lstKeyword.contains(arrKeyword[count])) {
-                lstKeyword.add(arrKeyword[count]);
+
+        for (char anArrKeyword : arrKeyword) {
+            if (!lstKeyword.contains(anArrKeyword)) {
+                lstKeyword.add(anArrKeyword);
             }
         }
 
-        for (int count = 0; count < alfabeto.length; count++) {
-            if (!lstKeyword.contains(alfabeto[count])) {
-                lstKeyword.add(alfabeto[count]);
+        for (char anAlfabeto : alfabeto) {
+            if (!lstKeyword.contains(anAlfabeto)) {
+                lstKeyword.add(anAlfabeto);
             }
         }
 
@@ -44,5 +97,26 @@ public class Playfair {
                 this.matriz[i][j] = lstKeyword.get(i * 5 + j);
             }
         }
+    }
+
+    private void preparaTextoClaro() {
+        String strTemp = this.textoClaro.toLowerCase().replaceAll("\\s", "");
+
+        for (int i = 0; i < strTemp.length()-1; i+=2) {
+            if (strTemp.charAt(i) == strTemp.charAt(i + 1)) {
+                lstTextoPreparado.add(strTemp.charAt(i));
+                lstTextoPreparado.add(x);
+                i--;
+            } else {
+                lstTextoPreparado.add(strTemp.charAt(i));
+                lstTextoPreparado.add(strTemp.charAt(i + 1));
+            }
+        }
+        this.strTextoPreparado = strTemp;
+    }
+
+    private void cifrarTexto() {
+        List<char[]> lstMatriz = Arrays.asList(this.matriz);
+        System.out.println(lstMatriz);
     }
 }
--- test/PlayfairTest.java
+++ test/PlayfairTest.java
@@ -2,19 +2,23 @@ import com.luancomputacao.cifras.Playfair;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by luan on 28/04/15.
+ *
+ * Test base to Playfair class
  */
 public class PlayfairTest {
 
     private Playfair playfair;
     private String keyworkd = "Monarchy";
-    private String textoClaro = "Luannb";
-    char[][] matrizmodel = {
+    private String textoClaro = "Luan nnb";
+    private String textoPreparado = "luannxnb";
+    private Playfair playfair2;
+    private String textoClaro2 = "balloon";
+    private String textoPreparado2 = "balxloon";
+    private char[][] matrizmodel = {
             {'m', 'o', 'n', 'a', 'r'},
             {'c', 'h', 'y', 'b', 'd'},
             {'e', 'f', 'g', 'i', 'k'},
@@ -27,6 +31,7 @@ public class PlayfairTest {
     @Before
     public void inicializa() {
         this.playfair = new Playfair(keyworkd, textoClaro);
+        this.playfair2 = new Playfair(keyworkd, textoClaro2);
     }
 
     @Test
@@ -38,17 +43,20 @@ public class PlayfairTest {
 
     public void imprimeMatrizMaked() {
         for (char[] aMatrizMaked : this.matrizMaked) {
-            for (int j = 0; j < aMatrizMaked.length; j++) {
-                System.out.print(aMatrizMaked[j]);
+            for (char anAMatrizMaked : aMatrizMaked) {
+                System.out.print(anAMatrizMaked);
             }
             System.out.println("");
         }
     }
 
-
     @Test
-    public void deveRetornarTextoPreparado(){
-
+    public void deveRetornarTextoPreparado1() {
+        assertEquals(this.textoPreparado, this.playfair.getStrTextoPreparado());
     }
 
+    @Test
+    public void deveRetornarTextoPreparado2() {
+        assertEquals(this.textoPreparado2, this.playfair2.getStrTextoPreparado());
+    }
 }
